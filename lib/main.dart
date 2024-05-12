c4b3d9fad7dbcba24daaf038d124df5ae00131d0
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-12 01:39:07 +0100

    Removed quiz-logo.png image file.
    This commit shows the deletion of the file "quiz-logo.png" from the "assets/images/" directory. The file was previously tracked in version control, but

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import "package:quiz_app/start_screen_container.dart";
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 112, 35, 245),
+                Color.fromARGB(255, 138, 86, 226)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
