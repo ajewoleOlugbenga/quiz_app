ea1d0b0bb18d5db8b0ac25684d6bb1616b931bf3
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-12 01:38:58 +0100

    Delete quiz-logo.png and update main.dart to use a gradient background for StartScreen
    The changes include:
    - Deletion of the quiz-logo.png file in the assets/images directory
    - Update

--- lib/start_screen_container.dart
+++ lib/start_screen_container.dart
@@ -4,6 +4,16 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Text("Hello screen");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'asset/images/_4e3cee06-d72d-4557-8029-e0424e005342.jpg',
+            width: 300,
+          )
+        ],
+      ),
+    );
   }
 }
