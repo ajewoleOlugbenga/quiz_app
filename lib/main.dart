89afff7ebfae9a9de3018bdbbd1ad210abc47992
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-14 06:09:08 +0100

    Update main.dart to run Quiz app
    * Replace `StartScreenContainer` with `Quiz` in `main.dart`
    * Import `quiz.dart` in `main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import "package:flutter/material.dart";
+import "package:quiz_app/quiz.dart";
 import "package:quiz_app/start_screen_container.dart";
 
 void main() {
-  runApp(
-    
-  );
+  runApp(const Quiz());
 }
