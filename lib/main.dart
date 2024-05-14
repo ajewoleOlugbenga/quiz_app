056daa740e29d2104a69b84f1e9f2d2150ba5bec
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-14 05:56:37 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,6 @@ import "package:quiz_app/start_screen_container.dart";
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 112, 35, 245),
-                Color.fromARGB(255, 138, 86, 226)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    
   );
 }
