20d88edde3ffadbb35ee1e2f317f661958aad21e
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-18 23:15:24 +0100

    "Updated QuestionContainer widget to display a column with a text and three elevated buttons."

--- lib/questions_container.dart
+++ lib/questions_container.dart
@@ -1,8 +1,8 @@
 import "package:flutter/material.dart";
 
 class QuestionContainer extends StatefulWidget {
- const QuestionContainer({super.key});
-  @override 
+  const QuestionContainer({super.key});
+  @override
   State<QuestionContainer> createState() {
     return _QuestionContainerState();
   }
@@ -11,6 +11,17 @@ class QuestionContainer extends StatefulWidget {
 class _QuestionContainerState extends State<QuestionContainer> {
   @override
   Widget build(context) {
-    return const Text("Questionnaire");
+    return Column(
+      children: [
+        const Text("The question..."),
+        const SizedBox(
+          height: 30,
+        ),
+        ElevatedButton(onPressed: () {}, 
+        child: const Text("Answer 1")),
+        ElevatedButton(onPressed: () {}, child: const Text("Answer 2")),
+        ElevatedButton(onPressed: () {}, child: const Text("Answer 3")),
+      ],
+    );
   }
 }
