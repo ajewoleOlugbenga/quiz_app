5ac4b65f4fa779dc415fb05ff46bd6079e4c252c
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-19 12:25:47 +0100

    "Refactored AnswerButton widget and updated QuestionContainer to display questions and answers from data source"

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  AnswerButton(this.answers,this.onTap,{super.key});
+  const AnswerButton(this.answers, this.onTap, {super.key});
 
   final String answers;
-final void Function() onTap;
+  final void Function() onTap;
   @override
   Widget build(BuildContext context) {
-  return ElevatedButton(onPressed: onTap,
-  style: ElevatedButton.styleFrom(), 
-          child: Text(answers));
-}}
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+          backgroundColor: const Color.fromARGB(255, 1, 52, 94),
+          foregroundColor: Colors.white, 
+        ),
+        
+        child: Text(answers),
+      ),
+    );
+  }
+}
