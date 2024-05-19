0bd54c9408fe946bb333fa916f18e166930d19f2
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-19 13:15:16 +0100

    "Refactor QuestionContainer widget to use Container with margin and adjusted Column layout"

--- lib/questions_container.dart
+++ lib/questions_container.dart
@@ -16,18 +16,25 @@ class _QuestionContainerState extends State<QuestionContainer> {
     final currentQuestions = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(currentQuestions.questions[0],
-              style: const TextStyle(color: Colors.white)),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestions.answers.map((answer) {
-            return AnswerButton(answer, () {});
-          })
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(currentQuestions.questions[0],
+                style: const TextStyle(color: Colors.white),
+                textAlign:TextAlign.center,
+                ),
+                
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestions.answers.map((answer) {
+              return AnswerButton(answer, () {});
+            })
+          ],
+        ),
       ),
     );
   }
