ec9adccc66ddab1ea7a46e88b5e6f4aac98f11f8
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-05-19 13:37:58 +0100

    "Added TextAlign.center to Text widget in AnswerButton"

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -17,7 +17,7 @@ class AnswerButton extends StatelessWidget {
           foregroundColor: Colors.white, 
         ),
         
-        child: Text(answers),
+        child: Text(answers, textAlign: TextAlign.center,),
       ),
     );
   }
